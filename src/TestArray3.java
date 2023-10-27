3ef9665047849f838c3aea10a8b5c7c5be41c926
obre-bjorn <obre.bjorn@gmail.com>
2023-10-27 18:41:06 +0300

    Array basics

---
+++ src/TestArray3.java
@@ -0,0 +1,65 @@
+public class TestArray3 {
+    public static void main(String args[]){
+
+
+        // Initialize
+        int arr1 [][] =  new int [3][3] ;
+
+        // Then declare
+
+        arr1[0][0]=1;
+
+        arr1[0][1]=2;
+
+        arr1[0][2]=3;
+
+        arr1[1][0]=4;
+
+        arr1[1][1]=5;
+
+        arr1[1][2]=1;
+
+        arr1[2][0]=2;
+
+        arr1[2][1]=3;
+
+        arr1[2][2]=4;
+
+         System.out.println();
+
+        for(int i=0;i<3;i++){
+
+        for(int j=0;j<3;j++){
+
+            System.out.print(arr1[i][j]+" ");
+
+        }
+
+        System.out.println();
+
+    }
+
+        
+
+//declaring and initializing 2D array
+
+int arr[][]={{1,2,3},{2,4,5},{4,4,5}};
+
+//printing 2D array
+System.out.println();
+
+    for(int i=0;i<3;i++){
+
+        for(int j=0;j<3;j++){
+
+            System.out.print(arr[i][j]+" ");
+
+        }
+
+        System.out.println();
+
+    }
+System.out.println();
+
+}}
+
---
+++ src/Testarray1.java
@@ -0,0 +1,49 @@
+
+/**
+ * InnerTestarray1
+ */
+public class Testarray1 {
+
+    public static void main(String args[]){
+
+        int a[] = new int[5];// declaration and instantiation
+
+        a[0] = 10;// initialization
+
+        a[1] = 20;
+
+        a[2] = 70;
+
+        a[3] = 40;
+
+        a[4] = 50;
+
+        // traversing array
+
+        for (int i = 0; i < a.length; i++)// length is the property of array
+
+            System.out.println(a[i]);
+
+        // OR
+    
+    int arr[]={33,3,4,5};//declaration, instantiation and initialization
+    
+    //printing array
+    
+    for(int i=0;i<arr.length;i++)//length is the property of array
+    
+    System.out.println(arr[i]);
+
+    
+
+// printing array using for-each loop
+
+for (int i : arr)
+
+    System.out.println(i);
+    
+    }
+    
+    //OR FOR EACH
+    
+}
